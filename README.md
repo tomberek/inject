28e48819f56b2d693371dd5676fa62520c76d81d
Tom Bereknyei <tom@dds.mil>
2019-01-05 18:03:50 -0500

    Idempotent checking without results required

--- README.md
+++ README.md
@@ -8,10 +8,16 @@ Requires a `sshloginfile` with a list of IPs. We assume you have a passwordless 
 
 Example usage:
 ```
-./inject {1..2} -- f*
-./inject 1 2 -- f1 f2
-./inject 1 -- f1
-./inject --slf another_sshloginfile {1..20} -- flags*
+./inject {1..2} ::: f*
+./inject 1 2 ::: f1 f2
+./inject 1 ::: f1
+./inject --slf another_sshloginfile {1..20} ::: flags ::: default
+./inject 1 ::: f2 ::: clean
+```
+
+Check answers:
+```
+./inject -l 1 ::: f2
 ```
 
 Saves results into the `last_run` directory.
@@ -19,3 +25,21 @@ Saves results into the `last_run` directory.
 ## Dependencies
 
 parallel, make, envsubst
+
+## SSH-based answer checker
+
+Config file at ./sshd_checker.config
+
+### Usage
+JUMPID ::: FLAGDIR ::: STRING
+Compares the contents of last_run/flag/FLAGDIR/target/[JUMP ssh command]/stdout to the STRING
+Attempts to strip .. and force relative path traversal
+
+Example server: `sudo /usr/sbin/sshd -f sshd_checker.config -p 1234`
+Example query : `ssh user@localhost -p 1234 1 ::: f1 ::: some_flag_text`
+
+Assumes the flags are defined in ~/inject/ and the results are in ~/inject/last_run
+Assumes jumphosts are defined in ~/inject/sshloginfile
+
+Results structure:
+last_run/flag/<flag>/maketarget/target/ssh -J root@<jumphost> root@<target>/stdout
--- inject.sh
+++ inject.sh
@@ -4,6 +4,7 @@ sshloginfile="sshloginfile"
 VERBOSE=
 RESULTS=last_run
 WORKINGDIR=
+REMOTE="-S {target} --transferfile {flag} --wd ..." 
 while true; do
     case "$1" in 
     -h|--help)
@@ -15,11 +16,12 @@ while true; do
         echo '    and to target specified by [flag_directories]/target'
         echo ''
         echo 'Options:'
-        echo '  -h --help       Show this help'
-        echo '  -v --verbose    Show verbose runs of parallel'
+        echo '  -h|--help       Show this help'
+        echo '  -v|--verbose    Show verbose runs of parallel'
         echo '  --slf file      Override sshloginfile [default: ./sshloginfile]'
         echo '  -r results_dir  Set path to store results in'
         echo '  -C dir          Set working directory, also applies to relative results'
+        echo '  -l|--local      Run makefile locally, good for checking flags'
         echo ''
         echo '    machine_indices: 1-indexed list to use [default: uses entire sshloginfile]'
         echo '    flag_directories: flag directories [default: all in working dir except results_dir]'
@@ -40,6 +42,10 @@ while true; do
     -r|--results)
         RESULTS="$2"
         shift
+        shift
+        ;;
+    -l|--local)
+        REMOTE=
         shift
         ;;
     -C)
@@ -82,7 +88,7 @@ fi
 printf %s\\n "$slf" | \
     ( printf %s\\n target,flag,maketarget  ; parallel -a - "parallel -I {] echo ssh -J root@{1} root@{1],{2},{3} :::: {2}/target" ::: $args ) | \
     parallel -j+0 --header : --results "$RESULTS" -M $VERBOSE -a - --colsep , \
-        "parallel $VERBOSE -j1 -n0 -M -S {target} --transferfile {flag} --wd ... make -s -C {flag} {maketarget} MACHINE='{target}' FLAG='{flag}' ::: 1"
+        "parallel $VERBOSE -j1 -n0 -M $REMOTE make -s -C {flag} {maketarget} MACHINE='{target}' FLAG='{flag}' ::: 1"
 
     # Beginings of attempt to fuse the parallel calls
     #( parallel -a - "parallel -I {] echo ssh -J root@{1} root@{1] :::: {2}/target" ::: $args ) | \
