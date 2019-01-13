289ec5ddd6ff1d033b4743197dbb47aab0fea790
Tom Bereknyei <tom@dds.mil>
2019-01-12 20:51:48 -0500

    Total rework, no sshloginfile, instead it is generated from flag_dir/target, see new syntax

--- inject.sh
+++ inject.sh
@@ -1,192 +1,108 @@
 #!/usr/bin/env bash
-
+function usage(){
+    echo 'Usage:'
+    echo '  inject.sh [options] ::: machine_indices ::: flag_directories ::: make_targets'
+    echo ''
+    echo '    Inject runs make in the [flag_directories] on target machines'
+    echo '    uses GNU parallel to ssh onto machines using hosts defined in flag_directories/target'
+    echo '    and to target specified by [flag_directories]/target'
+    echo ''
+    echo 'Options:'
+    echo '  -h|--help       Show this help'
+    echo '  -v|--verbose    Show verbose runs of parallel'
+    echo '  -r results_dir  Set path to store results in'
+    echo '  -C dir          Set working directory, also applies to relative results'
+    echo '  -l|--local      Run makefile locally, good for checking flags'
+    echo '  -d|--dry-run    Dry run, use with --verbose to debug CLI parsing'
+    echo ''
+    echo '    machine_indices: list to use as JUMP variable in the target templates'
+    echo '    flag_directories: flag directories [default: all in working dir except results_dir]'
+    echo '    make_targets: a list of make targets to call [default: default]'
+    echo ''
+    echo 'Env vars:'
+    echo '    MACHINE: the machine index in use [ in flag_dir/Makefile]'
+    echo '    FLAG: the flag directory in use [ in flag_dir/Makefile]'
+    echo '    JUMP: populated by the first argument to inject [ in flag_dir/target]'
+    echo ''
+    echo 'Examples: inject.sh ::: {1..2} ::: f* ::: default'
+    echo '          inject.sh ::: {1..2} ::: f* ::: default'
+    echo '          inject.sh ::: 1 2 :::+ f1 ::: default'
+    echo 'Version: 1.0'
+    exit 0
+}
 # Defaults
-POSITIONAL=()
-FLAGS=()
-sshloginfile="./sshloginfile"
 VERBOSE=
 RESULTS=last_run
 WORKINGDIR=
-REMOTE=1
-FIRST_OP=":::"
-SECOND_OP=":::"
+export REMOTE="--transferfile . --wd ..."
 DRY_RUN=
-
-while true; do
-    case "$1" in 
-    -h|--help)
-        echo 'Usage:'
-        echo '  inject.sh [options] [--slf sshloginfile] [machine_indices] [::: flag_directories [::: make_targets]]'
-        echo ''
-        echo '    Inject runs make in the [flag_directories] on target machines'
-        echo '    uses GNU parallel to ssh onto machines using jump hosts defined in sshloginfile'
-        echo '    and to target specified by [flag_directories]/target'
-        echo ''
-        echo 'Options:'
-        echo '  -h|--help       Show this help'
-        echo '  -v|--verbose    Show verbose runs of parallel'
-        echo '  --slf file      Override sshloginfile [default: ./sshloginfile]'
-        echo '  -r results_dir  Set path to store results in'
-        echo '  -C dir          Set working directory, also applies to relative results'
-        echo '  -l|--local      Run makefile locally, good for checking flags'
-        echo '  -d|--dry-run    Dry run, use with --verbose to debug CLI parsing'
-        echo ''
-        echo '    machine_indices: 1-indexed list to use [default: uses entire sshloginfile]'
-        echo '    flag_directories: flag directories [default: all in working dir except results_dir]'
-        echo '    make_targets: a list of make targets to call [default: default]'
-        echo ''
-        echo 'Env vars:'
-        echo '    MACHINE: the machine index in use [ in flag_dir/Makefile]'
-        echo '    FLAG: the flag directory in use [ in flag_dir/Makefile]'
-        echo '    JUMP: populated by sshloginfile [ in flag_dir/target]'
-        echo ''
-        echo 'Examples: inject.sh {1..2} ::: f*'
-        echo '          inject.sh {1..2} ::: f* ::: default'
-        echo '          inject.sh 1 2 :::+ f1'
-        echo 'Version: 0.2'
-        exit
-        ;;
-    -v|--verbose)
-        VERBOSE="-v"
-        shift
-        ;;
-    -r|--results)
-        RESULTS="$2"
-        shift
-        shift
-        ;;
-    -d|--dry-run)
-        DRY_RUN=1
-        shift
-        ;;
-    -l|--local)
-        REMOTE=
-        shift
-        ;;
-    -C)
-        WORKINGDIR="$2"
-        shift
-        shift
-        ;;
-    --slf)
-        sshloginfile="$2"
-        shift
-        shift
-        ;;
-    :::|:::+|::::|::::+)
-		FIRST_OP="$1"
-        shift
-        break
-        ;;
-    "")
-        break
-        ;;
-    *)
-        POSITIONAL+=("$1")
-		shift
-		;;
-	esac
-done
-
-while true; do
-    case "$1" in 
-    :::|:::+|::::|::::+)
-		SECOND_OP="$1"
-        shift
-        break
-        ;;
-    "")
-        break
-        ;;
-    *)
-        FLAGS+=("$1")
-		shift
-		;;
+while getopts r:C:hvdl FLAG; do
+    case $FLAG in
+        v) VERBOSE="-v" ;;
+        r) RESULTS="${OPTARG}" ;;
+        C) WORKINGDIR="${OPTARG}" ;;
+        d) DRY_RUN="--dry-run" ;;
+        l) REMOTE="" ;;
+        h) usage ;;
+        *) echo error ; break ;;
     esac
 done
-args="$@"
-sshloginfile="$(realpath "$sshloginfile")"
+shift $(expr $OPTIND - 1 )
+
+if [ "$#" -lt "6" ]; then
+    echo "Not enough arguments" >&2
+    usage
+fi
 
 # Jump to working directory
 if [ -n "$WORKINGDIR" ]; then
     pushd "$WORKINGDIR" >/dev/null
 fi
 
-# Default behavior when not specifying machine_ids
-if [ ${#POSITIONAL[@]} -eq 0 ]; then
-	ids=`wc -l "$sshloginfile" | cut -d' ' -f1`
-	ids=`seq 1 $ids`
-    echo "Default machines: $(echo $ids | tr '\n' ' ')" >&2
-else
-	ids="${POSITIONAL[@]}"
-fi
-
-# Default behavior when not specifying flags_directories
-if [ "${#FLAGS[@]}" -eq 0 ]; then
-    flags=`find * -maxdepth 0 -not -path "$RESULTS" -not -path '*/\.*' -type d`
-    echo "Default flags: $(echo $flags | tr '\n' ' ')" >&2
-else
-	flags="${FLAGS[@]}"
-fi
-
-# Default behavior when not specifying maketargets
-if [ -z "$args" ]; then
-	args="default"
-fi
-
-variations=`parallel "echo {1}$'\034'{2}$'\034'{3}" ::: $ids $FIRST_OP $flags $SECOND_OP $args`
-
 # Takes all variations and processes them into:
 # id jumphost flag maketarget
-# reads $sshloginfile in order to convert machine_id's to use as JUMP
+# uses the first arg as JUMP
 # in FLAG_DIR/target templating
-processed="$(gawk '
-BEGIN {
-    OFS=FS="\034"
-    print "flag","machine","maketarget"
+function go(){
+    printf "$1\034$4\034$5@"
+    printf "$1\034$3\034$5\\n" | tr -d '/'
 }
-FNR==NR{
-    jump[NR]=$0
+export -f go
+function run_parallel(){
+    parallel "parallel -I {] go {1] {2] {2/} {2} {3] ::: {1} :::: {2}/target ::: \$([ -z \"{3}\" ] && echo default || echo {3})" "$@"
 }
-FNR!=NR{
-    $2 = gensub(/\.\./,"","g",$2)
-    while(( getline a < ($2 "/target")) >0 ){
-        ENVIRON["JUMP"]=$1 #jump[$1]
-        print a |& "envsubst"
-        close("envsubst","to")
-        "envsubst" |& getline b
-        close("envsubst")
-        hostgroup = $2 OFS $1 OFS $3
-        hostgroup = gensub(/\//,"","g",hostgroup)
-        print "@" hostgroup "/1/" b
-        print $2,$1,$3 "@" hostgroup
-    }
-    close($2 "/target")
-}' "$sshloginfile" <(printf %s "$variations"))"
 
-newargs=`echo "$processed" | awk 'NR%2==1'`
-slf=`echo "$processed" | awk 'NR%2==0'`
+args="$(
+    printf $'%s\034%s\034%s\\n' "machine" "flag" "maketarget" ; 
+    run_parallel "$@"
+)"
+
+function go(){
+    local JUMP="$1"
+    export JUMP
+    printf "@$1\034$3\034$5" | tr -d '/'
+    if [ -z "$REMOTE" ]; then
+        host=":"
+    else
+        host="$2"
+    fi
+    printf "/1/$host\\n" | envsubst
+}
+slf="$(run_parallel "$@")"
 
 [ -n "$VERBOSE" ] && \
-	printf %s\\n "$newargs" | tr $'\034' '-'
+	printf "args:\\n%s\\n\\n" "$args" | tr $'\034' '-' >&2
 [ -n "$VERBOSE" ] && \
-	printf %s\\n "$slf" | tr $'\034' '-' | tr $'\035' '_'
+	printf "slf:\\n%s\\n\\n" "$slf" | tr $'\034' '-' | tr $'\035' '_' >&2
 [ -n "$DRY_RUN" ] && exit
 
 # The core
-if [ -z "$REMOTE" ]; then
-    echo "$newargs" | \
-    cut -d'@' -f1 | \
-    parallel --results "$RESULTS" --header : --colsep $'\034' $VERBOSE -M \
-        --hgrp -j1 \
-        make -s -C {flag} {maketarget} FLAG={flag/} MACHINE={machine}
-else
-    echo "$newargs" | \
-    parallel --results "$RESULTS" --header : --colsep $'\034' $VERBOSE -M \
-        --hgrp -j1 \
-        --slf <(printf %s\\n "$slf") --transferfile . --wd ... \
-        make -s -C {flag} {maketarget} FLAG={flag/} MACHINE={machine}
-fi
+echo "$args" | \
+parallel --results "$RESULTS" --header : --colsep $'\034' $VERBOSE -M \
+    --hgrp -j1 \
+    $REMOTE \
+    --slf <(printf %s\\n "$slf") \
+    make -s -C {flag} {maketarget} FLAG={flag/} MACHINE={machine}
 
 # Restore original directory
 if [ -n "$WORKINGDIR" ]; then
