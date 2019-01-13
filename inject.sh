bc39eaa2a583043804ada6361af18318d2cc3ec0
Tom Bereknyei <tom@dds.mil>
2019-01-13 18:05:14 -0500

    New API, closer to parallel native

--- inject.sh
+++ inject.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 function usage(){
     echo 'Usage:'
-    echo '  inject.sh [options] ::: machine_indices ::: flag_directories ::: make_targets'
+    echo '  inject.sh [options] ::: machine_indices :::: flag_targets ::: make_targets'
     echo ''
     echo '    Inject runs make in the [flag_directories] on target machines'
     echo '    uses GNU parallel to ssh onto machines using hosts defined in flag_directories/target'
-    echo '    and to target specified by [flag_directories]/target'
+    echo '    and to target specified by flag_target'
     echo ''
     echo 'Options:'
-    echo '  -h|--help       Show this help'
-    echo '  -v|--verbose    Show verbose runs of parallel'
+    echo '  -h              Show this help'
+    echo '  -v              Show verbose runs of parallel'
     echo '  -r results_dir  Set path to store results in'
     echo '  -C dir          Set working directory, must be "greatest common directory" of flags'
-    echo '  -l|--local      Run makefile locally, good for checking flags'
-    echo '  -d|--dry-run    Dry run, use with --verbose to debug CLI parsing'
+    echo '  -l              Run makefile locally, good for checking flags'
+    echo '  -d              Dry run, use with --verbose to debug CLI parsing'
     echo ''
     echo '    machine_indices: list to use as JUMP variable in the target templates'
     echo '    flag_directories: flag directories [default: all in working dir except results_dir]'
@@ -24,24 +24,24 @@ function usage(){
     echo '    FLAG: the flag directory in use [ in flag_dir/Makefile]'
     echo '    JUMP: populated by the first argument to inject [ in flag_dir/target]'
     echo ''
-    echo 'Examples: inject.sh ::: {1..2} ::: f* ::: default'
-    echo '          inject.sh ::: {1..2} ::: f* ::: default'
-    echo '          inject.sh ::: 1 2 :::+ f1 ::: default'
-    echo '          inject.sh -C folder ::: 1 2 ::: folder/f* ::: default'
-    echo 'Version: 1.1'
+    echo 'Examples: inject.sh ::: {1..2} :::: f*/target ::: default'
+    echo '          inject.sh ::: {1..2} :::: f*/target ::: default'
+    echo '          inject.sh ::: 1 2 ::::+ f1/target ::: default'
+    echo '          inject.sh -C folder ::: 1 2 :::: folder/f* ::: default'
+    echo 'Version: 2.0'
     exit 0
 }
 
 # Defaults
 VERBOSE=
-RESULTS=last_run
+RESULTS=
 export WORKINGDIR=
 export REMOTE="--transferfile . --wd ..."
 DRY_RUN=
 while getopts r:C:hvdl FLAG; do
     case $FLAG in
         v) VERBOSE="-v" ;;
-        r) RESULTS="${OPTARG}" ;;
+        r) RESULTS="--results ${OPTARG}" ;;
         C) WORKINGDIR="${OPTARG}" ;;
         d) DRY_RUN="--dry-run" ;;
         l) REMOTE="" ;;
@@ -56,47 +56,33 @@ if [ "$#" -lt "6" ]; then
     usage
 fi
 
-# Takes all variations and processes them into:
-# id jumphost flag maketarget
-# uses the first arg as JUMP
-# in FLAG_DIR/target templating
-
+# Takes all variations and processes them into arguments
 function go(){
-    printf "$1\034$3\034$4@"
-    printf "$1\034$3\034$4\\n" | tr -d '/.'
+    local host
+    if [ -z "$REMOTE" ]; then
+        host=":"
+    else
+        host=$(printf "$1" | tr -d '/.')
+    fi
+    echo "$@" >&2
+    printf "$3@$host"
+    printf "\t$4@$host"
+    printf "\t$1@$host\\n"
 }
 
-# Function tries to be smart and detect globing from shell, or directories relative to WORKINGDIR
-function get_name(){
-    local file
-    file="$(realpath "$1" 2>/dev/null)"
-    if [ -f "$file" ] ; then
-        printf %s "$file"
-        return
-    fi
-    file="$(realpath "$WORKINGDIR"/"$1" 2>/dev/null)"
-    if [ -f "$file" ] ; then
-        printf %s "$file"
-        return
-    fi
-    echo "Unknown flag folders" >&2
-    exit
-}
-export -f get_name
 export -f go
+# Perl magic is to extract the flag's name
 function run_parallel(){
-    parallel "parallel -I {] go {1] {2] {2/} {3} ::: {1} :::: \$( get_name {2}/target) " "$@"
+    parallel --will-cite --plain go {1} {2} {=2 '$_=::basename(::dirname($opt::a[1]))' =} {3} "$@"
 }
 
-args="$(
-    printf $'%s\034%s\034%s\\n' "machine" "flag" "maketarget" ; 
-    run_parallel "$@"
-)"
+args="$( run_parallel "$@")"
 
 function go(){
+    local host
     local JUMP="$1"
     export JUMP
-    printf "@$1\034$3\034$4" | tr -d '/.'
+    printf "@$1" | tr -d '/.'
     if [ -z "$REMOTE" ]; then
         host=":"
     else
@@ -107,9 +93,9 @@ function go(){
 slf="$(run_parallel "$@")"
 
 [ -n "$VERBOSE" ] && \
-	printf "args:\\n%s\\n\\n" "$args" | tr $'\034' '-' >&2
+	printf "arguments:\\n%s\\n\\n" "$args" >&2
 [ -n "$VERBOSE" ] && \
-	printf "slf:\\n%s\\n\\n" "$slf" | tr $'\034' '-' | tr $'\035' '_' >&2
+	printf "sshloginfile:\\n%s\\n\\n" "$slf" >&2
 [ -n "$DRY_RUN" ] && exit
 
 if [ -n "$WORKINGDIR" ]; then
@@ -117,12 +103,14 @@ if [ -n "$WORKINGDIR" ]; then
 fi
 
 # The core
-echo "$args" | \
-parallel -k --hgrp --results "$RESULTS" --header : --colsep $'\034' \
-    $REMOTE $VERBOSE \
-    -j4 -M \
-    --slf <(printf %s\\n "$slf") \
-    make -s -C {flag} {maketarget} FLAG={flag/} MACHINE={machine}
+arg1=$(echo "$args" | cut -f1)
+arg2=$(echo "$args" | cut -f2)
+arg3=$(echo "$args" | cut -f3)
+
+parallel --hgrp --header : --slf <(printf %s "$slf") \
+    --will-cite -j+0 -k --group -M --sshdelay 0.2 \
+    $INJECT_PARALLEL_ARGS $REMOTE $VERBOSE $RESULTS \
+    make -s -C {1} {2} FLAG={1/} MACHINE={3} ::: "$(echo "$arg1")" :::+ "$(echo "$arg2")" :::+ "$(echo "$arg3")"
 
 # Restore original directory
 if [ -n "$WORKINGDIR" ]; then
