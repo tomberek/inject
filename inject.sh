3cdeb1fee0f57286fab8b65c6da84aee1d522b25
Tom Bereknyei <tom@dds.mil>
2019-01-14 18:06:21 -0500

    -n -o feature bump

--- inject.sh
+++ inject.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+if [ "$_"x != "$0"x ]; then
+    echo "Script is being sourced"
+    return
+fi
 function usage(){
     echo 'Usage:'
     echo '  inject.sh [options] ::: machine_indices :::: flag_targets ::: make_targets'
@@ -10,6 +14,8 @@ function usage(){
     echo 'Options:'
     echo '  -h              Show this help'
     echo '  -v              Show verbose runs of parallel'
+    echo '  -s slf          Write ssshloginfile to slf'
+    echo '  -c cmd          Run cmd instead of make'
     echo '  -r results_dir  Set path to store results in'
     echo '  -C dir          Set working directory, must be "greatest common directory" of flags'
     echo '  -l              Run makefile locally, good for checking flags'
@@ -28,7 +34,7 @@ function usage(){
     echo '          inject.sh ::: {1..2} :::: f*/target ::: default'
     echo '          inject.sh ::: 1 2 ::::+ f1/target ::: default'
     echo '          inject.sh -C folder ::: 1 2 :::: folder/f* ::: default'
-    echo 'Version: 2.0'
+    echo 'Version: 2.1'
     exit 0
 }
 
@@ -38,10 +44,15 @@ RESULTS=
 export WORKINGDIR=
 export REMOTE="--transferfile . --wd ..."
 DRY_RUN=
-while getopts r:C:hvdl FLAG; do
+SLF=
+CMD="make -s -C {2} {3} FLAG={2/} MACHINE={1}"
+while getopts o:n:s:r:C:hvdl FLAG; do
     case $FLAG in
         v) VERBOSE="-v" ;;
+        s) SLF="${OPTARG}" ;;
         r) RESULTS="--results ${OPTARG}" ;;
+        o) CMD="${OPTARG}" ;;
+        n) CMD="-N0 ${OPTARG}" ;;
         C) WORKINGDIR="${OPTARG}" ;;
         d) DRY_RUN="--dry-run" ;;
         l) REMOTE="" ;;
@@ -51,7 +62,7 @@ while getopts r:C:hvdl FLAG; do
 done
 shift $(expr $OPTIND - 1 )
 
-if [ "$#" -lt "6" ]; then
+if [ "$#" -lt "4" ]; then
     echo "Not enough arguments" >&2
     usage
 fi
@@ -64,9 +75,9 @@ function go1(){
     else
         host=$(printf "$1" | tr -d '/.')
     fi
-    printf "$3@$host"
-    printf "\t$4@$host"
-    printf "\t$1@$host\\n"
+    printf "$1@$host"
+    printf "\t$3@$host"
+    printf "\t$4@$host\\n"
 }
 function go2(){
     local host
@@ -81,6 +92,7 @@ function go2(){
     printf "/1/$host\\n" | envsubst
 }
 function go(){
+    # Alternate lines, split them up later, cuts down on calls to parallel
     go1 "$@"
     go2 "$@"
 }
@@ -89,23 +101,19 @@ export -f go1
 export -f go2
 
 # Perl magic is to extract the flag's name
-function run_parallel(){
-    parallel --will-cite --plain go {1} {2} {=2 '$_=::basename(::dirname($opt::a[1]))' =} {3} "$@"
-}
-
-res="$( run_parallel "$@")"
+res="$( parallel --will-cite --plain go {1} {2} {=2 '$_=::basename(::dirname($opt::a[1]))' =} {3} "$@" )"
 args="$( printf %s "$res" | awk 'NR % 2 == 1')"
 slf="$( printf %s "$res" | awk 'NR % 2 == 0')"
 
-[ -n "$VERBOSE" ] && \
-	printf "arguments:\\n%s\\n\\n" "$args" >&2
-[ -n "$VERBOSE" ] && \
-	printf "sshloginfile:\\n%s\\n\\n" "$slf" >&2
+if [ -n "$SLF" ]; then
+    printf %s\\n "$slf" > "$SLF"
+fi
+
+[ -n "$VERBOSE" ] && printf "arguments:\\n%s\\n\\n" "$args" >&2
+[ -n "$VERBOSE" ] && printf "sshloginfile:\\n%s\\n\\n" "$slf" >&2
 [ -n "$DRY_RUN" ] && exit
 
-if [ -n "$WORKINGDIR" ]; then
-    pushd "$WORKINGDIR" >/dev/null
-fi
+[ -n "$WORKINGDIR" ] && pushd "$WORKINGDIR" >/dev/null
 
 # The core
 arg1=$(echo "$args" | cut -f1)
@@ -113,9 +121,9 @@ arg2=$(echo "$args" | cut -f2)
 arg3=$(echo "$args" | cut -f3)
 
 parallel --hgrp --slf <(printf %s "$slf") \
-    --will-cite -j+0 -k --group -M --sshdelay 0.2 \
+    --plain --will-cite -j+0 -k --group -M --sshdelay 0.2 \
     $INJECT_PARALLEL_ARGS $REMOTE $VERBOSE $RESULTS \
-    make -s -C {1} {2} FLAG={1/} MACHINE={3} ::: "$(echo "$arg1")" :::+ "$(echo "$arg2")" :::+ "$(echo "$arg3")"
+    $CMD ::: "$(echo "$arg1")" :::+ "$(echo "$arg2")" :::+ "$(echo "$arg3")"
 
 # Restore original directory
 if [ -n "$WORKINGDIR" ]; then
